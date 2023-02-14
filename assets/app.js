60c6bd8900f7bffadd8d211727959e8f5ce2fc6c
MATHIEU VAN DAMME <vandammemathieu@hotmail.fr>
2023-02-14 17:22:35 +0100

    add style on navbar

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,5 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import 'tw-elements';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.html.twig'],
+  content: [
+      './templates/**/*.html.twig',
+      './templates/admin/*.html.twig',
+      './node_modules/tw-elements/dist/js/**/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('tw-elements/dist/plugin')
+  ],
 }
