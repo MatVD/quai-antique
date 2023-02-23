8f78a7af0efe52f68238bcb13ae1b438c5b6ed8e
MatVD <vandammemathieu@hotmail.fr>
2023-02-23 13:18:24 +0100

    fix bug on js file: add btn close flash message in if statement

--- assets/js/flashMessage.js
+++ assets/js/flashMessage.js
@@ -1,18 +1,23 @@
 // ajout de fonctionnalités dynamiques sur flash-messages
 
+
 let btnClose = document.querySelector("#btn-close-flash")
 
-// Le flash message se ferme au click de l'utilsateur
-btnClose.onclick = ((e) => {
+// dans un if afin d'éviter un message d'erreur (TypeError: Cannot set properties of null (setting 'onclick'))
+if (btnClose) {
+    // Le flash message se ferme au click de l'utilsateur
+    btnClose.onclick = ((e) => {
         e.preventDefault();
 
         let popupDiv = document.querySelector('#flash-popup');
 
         popupDiv.style.display = 'none';
-});
+    });
 
 // Le flash message se ferme au bouts de 10s
-setInterval(() => {
-       btnClose.click();
-}, 15000)
+    setInterval(() => {
+        btnClose.click();
+    }, 15000)
+}
+
 
